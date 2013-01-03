8d6b5f287d51f1677dab8444fe3791c299c28f0a
Marcus Stollsteimer <sto.mar@web.de>
2013-01-03 11:14:07 +0100

    Update copyright information

--- lib/deblank.rb
+++ lib/deblank.rb
@@ -15,7 +15,7 @@
 #
 # == Author
 #
-# Copyright (C) 2012 Marcus Stollsteimer
+# Copyright (C) 2012-2013 Marcus Stollsteimer
 #
 # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 
@@ -30,7 +30,7 @@ module Deblank
   HOMEPAGE  = 'https://github.com/stomar/deblank'
   TAGLINE   = 'remove special characters from filenames'
 
-  COPYRIGHT = "Copyright (C) 2012 Marcus Stollsteimer.\n" +
+  COPYRIGHT = "Copyright (C) 2012-2013 Marcus Stollsteimer.\n" +
               "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n" +
               "This is free software: you are free to change and redistribute it.\n" +
               "There is NO WARRANTY, to the extent permitted by law."
