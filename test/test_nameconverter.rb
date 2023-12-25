4b0892c13c7151d96d5b71a9811276c53b71306a
Marcus Stollsteimer <sto.mar@web.de>
2023-12-25 19:40:55 +0100

    Avoid deprecation warnings for minitest expectations
    Use value wrappers for minitest expectations.
    Since minitest 5.12.0, using the old syntax with
    global expectations causes deprecation warnings.

--- test/test_nameconverter.rb
+++ test/test_nameconverter.rb
@@ -9,32 +9,32 @@ describe Deblank::NameConverter do
   end
 
   it 'recognizes invalid filenames' do
-    @nc.invalid?('path/to/filename with spaces.txt').must_equal true
-    @nc.invalid?('filename_with_ä.txt').must_equal true
-    @nc.invalid?('Valid_filename-1.txt').must_equal false
+    _(@nc.invalid?('path/to/filename with spaces.txt')).must_equal true
+    _(@nc.invalid?('filename_with_ä.txt')).must_equal true
+    _(@nc.invalid?('Valid_filename-1.txt')).must_equal false
   end
 
   it 'does not change the path name' do
-    @nc.convert('path with spaces/file.txt').must_equal 'path with spaces/file.txt'
+    _(@nc.convert('path with spaces/file.txt')).must_equal 'path with spaces/file.txt'
   end
 
   it 'replaces spaces by underscores' do
-    @nc.convert('file with spaces.txt').must_equal 'file_with_spaces.txt'
+    _(@nc.convert('file with spaces.txt')).must_equal 'file_with_spaces.txt'
   end
 
   it 'removes parentheses' do
-    @nc.convert('file_(another).txt').must_equal 'file_another.txt'
+    _(@nc.convert('file_(another).txt')).must_equal 'file_another.txt'
   end
 
   it 'transliterates umlauts and eszett' do
-    @nc.convert('Ä_Ö_Ü_ä_ö_ü_ß.txt').must_equal 'Ae_Oe_Ue_ae_oe_ue_ss.txt'
+    _(@nc.convert('Ä_Ö_Ü_ä_ö_ü_ß.txt')).must_equal 'Ae_Oe_Ue_ae_oe_ue_ss.txt'
   end
 
   it 'can return the default valid characters as string' do
-    Deblank::NameConverter.default_valid_chars_to_s.must_equal 'A-Z a-z 0-9 . _ -'
+    _(Deblank::NameConverter.default_valid_chars_to_s).must_equal 'A-Z a-z 0-9 . _ -'
   end
 
   it 'can return the default substitutions as string' do
-    Deblank::NameConverter.default_substitutions_to_s.split("\n")[0].must_equal '  => _'
+    _(Deblank::NameConverter.default_substitutions_to_s.split("\n")[0]).must_equal '  => _'
   end
 end
--- test/test_optionparser.rb
+++ test/test_optionparser.rb
@@ -13,29 +13,29 @@ describe Deblank::Optionparser do
 
   it 'can correct encoding from (seemingly) CP850 to Windows-1252' do
     arg = @arg_Win_1252_labeled_as_CP850
-    arg.encoding.must_equal Encoding::CP850
+    _(arg.encoding).must_equal Encoding::CP850
     corrected_arg = @parser.correct_encoding(arg)
-    corrected_arg.encoding.must_equal Encoding::Windows_1252
+    _(corrected_arg.encoding).must_equal Encoding::Windows_1252
   end
 
   it 'encodes (seemingly) CP850 encoded filenames into UTF-8' do
     options = @parser.parse!([@arg_Win_1252_labeled_as_CP850])
     filename = options[:files].first
-    filename.encoding.must_equal Encoding::UTF_8
-    filename.must_equal @arg
+    _(filename.encoding).must_equal Encoding::UTF_8
+    _(filename).must_equal @arg
   end
 
   it 'encodes Windows-1252 encoded filenames into UTF-8' do
     options = @parser.parse!([@arg_Win_1252])
     filename = options[:files].first
-    filename.encoding.must_equal Encoding::UTF_8
-    filename.must_equal @arg
+    _(filename.encoding).must_equal Encoding::UTF_8
+    _(filename).must_equal @arg
   end
 
   it 'encodes UTF-8 encoded filenames into UTF-8' do
     options = @parser.parse!([@arg])
     filename = options[:files].first
-    filename.encoding.must_equal Encoding::UTF_8
-    filename.must_equal @arg
+    _(filename.encoding).must_equal Encoding::UTF_8
+    _(filename).must_equal @arg
   end
 end
